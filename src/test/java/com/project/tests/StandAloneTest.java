55181c08b247dcd1af37a7b0335e2958a805f1c6
rodfernando <hazardrodrigo@hotmail.com>
2023-06-28 10:54:56 -0300

    First Commit

--- src/test/java/com/project/tests/StandAloneTest.java
+++ src/test/java/com/project/tests/StandAloneTest.java
@@ -1,2 +1,93 @@
-package com.project.tests;public class StandAloneTest {
+package com.project.tests;
+
+import com.project.utils.ChromeTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StandAloneTest extends ChromeTest {
+
+    @Test
+    void standAlone() throws InterruptedException {
+        String nomeDoProduto1 = "ADIDAS ORIGINAL";
+        String nomeDoProduto2 = "IPHONE 13 PRO";
+
+        driver.get("https://www.rahulshettyacademy.com/client");
+        driver.findElement(By.id("userEmail")).sendKeys("tester@example.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Rodrigotester01");
+        driver.findElement(By.id("login")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='toast-container']")));
+        // Assertion do toast de login
+        String loging = driver.findElement(By.xpath("//*[@id='toast-container']")).getText();
+        assertEquals(loging, "Login Successfully");
+
+        //Espera dos produtos aparecerem na tela, em específico o webelement:
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+
+        List<WebElement> itens = driver.findElements(By.cssSelector(".mb-3"));
+
+        WebElement item1 =  itens.stream()
+                .filter(i->i.findElement(By.cssSelector("b")).getText().equals(nomeDoProduto1))
+                .findFirst().orElse(null); // isso achará o card que tem o adidas original
+        item1.findElement(By.cssSelector(".card-body button:last-child")).click();
+
+        Thread.sleep(5000);
+
+        WebElement item2 =  itens.stream()
+                .filter(i->i.findElement(By.cssSelector("b")).getText().equals(nomeDoProduto2))
+                .findFirst().orElse(null);
+        item2.findElement(By.cssSelector(".card-body button:last-child")).click();
+
+
+
+        //buscando pelo toast após adicionar item no carrinho:
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='toast-container']")));
+        String msg = driver.findElement(By.xpath("//*[@id='toast-container']")).getText();
+        assertEquals(msg,"Product Added To Cart");
+
+        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+        //Quantidade de itens no carrinho
+        List<WebElement> itensCart = driver.findElements(By.cssSelector(".cartSection h3"));
+
+
+       boolean match1 = itensCart.stream().anyMatch(i->i.getText().equalsIgnoreCase(nomeDoProduto1));
+       boolean match2 = itensCart.stream().anyMatch(i->i.getText().equalsIgnoreCase(nomeDoProduto2));
+       assertTrue(match1);
+       assertTrue(match2);
+
+       /*Se eu quero encontrar um elemento usando stream, uso o filter. Caso queira somente verificar, uso anyMatch*/
+
+        driver.findElement(By.xpath("//button[normalize-space()='Checkout']")).click();
+
+
+
+        //Tela Checkout
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "Br").build().perform();
+        //Espera para aparecer a lista na tela:
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-item")));
+        driver.findElement(By.xpath("//span[normalize-space()='Brazil']")).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
+        String confirmOrder = driver.findElement(By.cssSelector("h1")).getText();
+        assertTrue(confirmOrder.equalsIgnoreCase("Thankyou for the order."));
+    }
+
+
 }
--- src/test/java/com/project/utils/ChromeTest.java
+++ src/test/java/com/project/utils/ChromeTest.java
@@ -1,2 +1,39 @@
-package com.project.utils;public class ChromeTest {
+package com.project.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ChromeTest {
+
+    public static WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //max timeout
+        driver.manage().deleteAllCookies();
+    }
+
+//    @AfterEach
+//    void teardown() {
+//        driver.quit();
+//    }
+
+    // Alternative
+//    @BeforeEach
+//    void setup() {
+//        driver = WebDriverManager.chromedriver().create();
+//
+//    }
 }
